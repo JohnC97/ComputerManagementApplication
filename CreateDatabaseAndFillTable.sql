78162da157d073f28b6424f3823f658cf5c0d9ac
OliverPersson <oliver-persson@hotmail.com>
2017-05-19 15:24:58 +0200

    New .sql

--- CreateDatabaseAndFillTable.sql
+++ CreateDatabaseAndFillTable.sql
@@ -1,3 +1,12 @@
+CREATE TABLE ComputerList (
+   ID        INTEGER AUTO_INCREMENT,
+   pcName    text,
+   pcType    text,
+   pcSpec    text,
+   pcPrice   text,
+   pcImgsrc  text,
+   PRIMARY KEY (ID)
+);
 INSERT INTO Computer(computerName, computerType, computerSpec, computerPrice, computerImgsrc)
 VALUES
 ('Lenovo Chromebook N22-20 11,6', 'HD Chromebook', 'Celeron N3060, 2GB RAM,16GB SSD, Google Chrome OS', '1.990:-', 'https://www.komplett.se/img/p/200/898888.jpg'),
@@ -192,4 +201,4 @@ VALUES
 ('Microsoft Surface Book 13,5', 'QHD+ 2-i-1 datorer', 'GeForce GTX965M, Core i7-6600U, 16GB RAM, 1TB SSD, Windows 10 Pro', '39.990:-', 'https://www.komplett.se/img/p/200/911323.jpg'),
 ('Lenovo P50 15,6', 'Workstation XEON UHD/4K Alla bärbara datorer', 'Quadro M2000M-4GB, XEON-1535M, 16GB RAM, 512GB SSD, Win7 Pro/Win10 Pro', '39.990:-', 'https://www.komplett.se/img/p/200/908571.jpg'),
 ('MSI GT83VR 18.4', 'Full HD matt G-SYNC Bärbar', 'GeForce GTX1080 SLI,Core i7-7820HK,64GB RAM,512GB PCIe SSD,1TB HDD, BD-RE,W10H', '49.990:-', 'https://www.komplett.se/img/p/200/907974.jpg'),
-('Acer Predator 21X 21', 'Curved FHD G-SYNC Bärbar', 'GeForce GTX1080 SLI,Core i7-7820HK,64GB RAM,1 TB PCIe SSD,1TB HDD,W10 Home', '94.990:-', 'https://www.komplett.se/img/p/200/911991.jpg');
+('Acer Predator 21X 21', 'Curved FHD G-SYNC Bärbar', 'GeForce GTX1080 SLI,Core i7-7820HK,64GB RAM,1 TB PCIe SSD,1TB HDD,W10 Home', '94.990:-', 'https://www.komplett.se/img/p/200/911991.jpg'),
