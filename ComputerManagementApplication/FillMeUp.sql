7e64e8418fe1aa25c5f302b6486958ed4537d32b
JohnC97 <john.claeson@gmail.com>
2017-05-21 19:59:06 +0200

    oh maj gadd

--- ComputerManagementApplication/FillMeUp.sql
+++ ComputerManagementApplication/FillMeUp.sql
@@ -8,9 +8,10 @@ CREATE TABLE Computer (
    computerImgsrc  varchar(255),
    PRIMARY KEY (ID)
 );
-*/
 
+*/
 -- After deploy
+
 
 INSERT INTO Computer(computerName, computerType, computerSpec, computerPrice, computerImgsrc)
 VALUES
@@ -207,3 +208,4 @@ VALUES
 ('Lenovo P50 15,6', 'Workstation XEON UHD/4K Alla bärbara datorer', 'Quadro M2000M-4GB, XEON-1535M, 16GB RAM, 512GB SSD, Win7 Pro/Win10 Pro', '39990', 'https://www.komplett.se/img/p/200/908571.jpg'),
 ('MSI GT83VR 18.4', 'Full HD matt G-SYNC Bärbar', 'GeForce GTX1080 SLI,Core i7-7820HK,64GB RAM,512GB PCIe SSD,1TB HDD, BD-RE,W10H', '49990', 'https://www.komplett.se/img/p/200/907974.jpg'),
 ('Acer Predator 21X 21', 'Curved FHD G-SYNC Bärbar', 'GeForce GTX1080 SLI,Core i7-7820HK,64GB RAM,1 TB PCIe SSD,1TB HDD,W10 Home', '94990', 'https://www.komplett.se/img/p/200/911991.jpg');
+
